f7b8d363bc202b4fd06cbf5effde7b0da80b81cb
Laura Wenderoth <laura.wenderoth@stud.uke.uni-hamburg.de>
2022-08-02 13:38:26 +0200

    FID also for test

--- test_patches.py
+++ test_patches.py
@@ -4,6 +4,7 @@ from options.test_options import TestOptions
 from data import create_dataset
 from models import create_model
 from util.logger import save_images, calculate_evaluation_metrices
+from util.logger import Logger
 
 from util.util import mkdir, tensor2im, save_image
 
@@ -33,7 +34,7 @@ if __name__ == '__main__':
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     model = create_model(opt)  # create a model given opt.model and other options
     model.setup(opt)  # regular setup: load and print networks; create schedulers
-
+    logger = Logger(opt)
     # initialize logger
     if opt.use_wandb:
         wandb_run = wandb.init(project='CycleGAN-and-pix2pix_results', name=opt.name, config=opt,
@@ -46,9 +47,9 @@ if __name__ == '__main__':
     mkdir(save_path)
     number_of_patches = opt.patches_per_width ** 2
     images = {}
-    SSMI_A = []
-    SSMI_B = []
+
     ims_dict = {}
+    # logger.log_evaluation_metrics(opt=opt, state="test", model=model, val_dataset=dataset)
     for patch_index, data in enumerate(dataset):
 
         model.set_input(data)  # unpack data from data loader
@@ -78,28 +79,13 @@ if __name__ == '__main__':
         img_path = model.get_image_paths()  # get image paths
         save_images(save_path, visuals, img_path, aspect_ratio=opt.aspect_ratio,
                     use_wandb=opt.use_wandb)
-        evaluation_metrics = calculate_evaluation_metrices(visuals, opt)
+        logger.log_evaluation_metrics(opt=opt,state="test",visuals=visuals)
         if opt.patches_per_width != 1:
             for key in visuals.keys():
                 patch = visuals[key]
                 key = key + " merged"
                 put_image_together(images[key], patch, patch_index, opt.patches_per_width)
 
-        if "SSMI_A" in evaluation_metrics.keys():
-            SSMI_A.append(evaluation_metrics["SSMI_A"])
-            if opt.use_wandb:
-                wandb_run.log({'validation SSMI_A': evaluation_metrics["SSMI_A"]})
-        if "SSMI_B" in evaluation_metrics.keys():
-            SSMI_B.append(evaluation_metrics["SSMI_B"])
-            if opt.use_wandb:
-                wandb_run.log({'validation SSMI_B': evaluation_metrics["SSMI_B"]})
-    if len(SSMI_A) > 0:
-        ssmi_a = np.array(SSMI_A)
-        ssmi_a = ssmi_a.mean()
-        if opt.use_wandb:
-            wandb_run.log({'validation SSMI_A mean': ssmi_a})
-    if len(SSMI_B) > 0:
-        ssmi_b = np.array(SSMI_B)
-        ssmi_b = ssmi_b.mean()
-        if opt.use_wandb:
-            wandb_run.log({'validation SSMI_B mean': ssmi_b})
+
+
+
