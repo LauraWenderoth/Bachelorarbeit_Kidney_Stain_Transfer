f866c6cd6645c4c5ac0ef0f74cd59e16dbf03d63
Laura Wenderoth <laura.wenderoth@stud.uke.uni-hamburg.de>
2022-08-10 11:13:22 +0200

    PARSER try to fix bug

--- options/train_options.py
+++ options/train_options.py
@@ -19,7 +19,7 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--epoch_count', type=int, default=1, help='the starting epoch count, we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>, ...')
         parser.add_argument('--phase', type=str, default='train', help='train, val, test, etc')
         # training parameters
-        parser.add_argument('--n_epochs', type=int, default=100, help='number of epochs with the initial learning rate')
+        parser.add_argument('--n_epochs', type=int, default=2, help='number of epochs with the initial learning rate')
         parser.add_argument('--n_epochs_decay', type=int, default=100, help='number of epochs to linearly decay learning rate to zero')
         parser.add_argument('--beta1', type=float, default=0.5, help='momentum term of adam')
         parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate for adam')
@@ -28,7 +28,7 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--lr_policy', type=str, default='linear', help='learning rate policy. [linear | step | plateau | cosine]')
         parser.add_argument('--lr_decay_iters', type=int, default=50, help='multiply by a gamma every lr_decay_iters iterations')
         #
-        parser.add_argument('--weight_for_L1_loss', type=list, default=[1,1,1],
+        parser.add_argument('--weight_for_L1_loss', metavar=('R','G','B'),  type=float, nargs=3, default=[0.1,2,5],
                             help='[R,G,B] channel is multiplied by weight before calculation of L1 loss')
         parser.add_argument('--reset_L1_loss', type=list, default=[1,1,1],
                             help='reset [R,G,B]  for the time in which the learning rate linearly decreases to zero')
