37e2ce38fdd8da25728247b61fd73b413687a030
Jan Strich <53601317+pesc101@users.noreply.github.com>
2022-08-26 13:09:32 +0200

    Add boxplot

--- graphics/boxplot.py
+++ graphics/boxplot.py
@@ -3,6 +3,10 @@ import pandas as pd
 import seaborn as sns
 import glob
 import matplotlib.pyplot as plt
+import numpy as np
+import tikzplotlib
+
+sns.set()
 
 # %%
 files = glob.glob('results_data/*')
@@ -20,9 +24,27 @@ for file in files:
     df_final = pd.concat([df_final, df])
 
 # %%
+labels = ['L1 Patches', 'L1 Reset Patches', 'Van Patches']
 
-df_plot = df_final[df_final.metric == 'MAE']
-sns.boxplot(data=df_plot, x='name', y='value')
-plt.xticks(rotation=15)
-plt.show()
 
+def boxplot(df: pd.DataFrame = df_final, metric: str = 'MAE', labels: list = []) -> None:
+    df_plot = df[df_final.metric == metric]
+    sns.boxplot(data=df_plot, x='name', y='value',
+                showmeans=True,
+                meanprops={"marker": "o",
+                           "markerfacecolor": "white",
+                           "markeredgecolor": "black",
+                           "markersize": "5"})
+    sns.set(rc={'figure.figsize': (15, 9)})
+    sns.set_style('whitegrid')
+    plt.xticks(np.arange(3), labels)
+    plt.xlabel('Type')
+    plt.ylabel(metric)
+    tikzplotlib.save(f"graphics/{metric}.tex", axis_height='8cm', axis_width='10.5cm')
+    plt.show()
+
+
+boxplot(df_final, 'FID', labels)
+boxplot(df_final, 'MAE', labels)
+boxplot(df_final, 'MSE', labels)
+boxplot(df_final, 'SSIM', labels)
